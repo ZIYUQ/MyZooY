660bcf8f60d8685c4059a9d1d4c1aead946c13c3
ShengjunSu <shengjuns@student.unimelb.edu.au>
2022-05-04 21:34:55 +0800

    make the framework for frontend

---
+++ frontend/src/pages/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component{
+    
+
+
+    render() {
+        return (
+            <h1>HI</h1>
+        )
+    }
+
+}
+
+export default HomePage;
