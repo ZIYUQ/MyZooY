57b94ec7ac5909503295a318dfc3480fd5101d53
ShengjunSu <shengjuns@student.unimelb.edu.au>
2022-05-04 23:02:47 +0800

    Update git ignore

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class HomePage extends React.Component{
-    
+    componentDidMount(){
+        fetch('http://localhost:8000/login')
+        .then(resp => {
+            return resp.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    }
 
 
     render() {
